6b1425119b611023c6d8c3cb1c7d7c72099c1ddb
Eitiane Borges <eitianeborges@gmail.com>
2025-03-06 23:10:00 -0300

    inicio do processo de consulta de dados

--- consulta-vendedor/consultarVendedor.js
+++ consulta-vendedor/consultarVendedor.js
@@ -1,12 +1,16 @@
 let mensagemErroConsulta = 'Erro a consultar dados';
+LOCAL_STORAGE_KEY = 'vendedores';
 
 function consultarVendedor() {
-    if (nomeVendedor == '') {
+    let vendedores = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    let tabela = document.querySelector('.tabela-vendedores');
+}
+    /* if (nomeVendedor == '') {
         alert(mensagemConsulta);
         return;
     } else {
         alert('consulta de vendedor')
         alert(`nome: ${nomeVendedor}\nIdade: ${idadeVendedor}\nVendas: ${vendasVendedor}\nCarga Horária: ${cargaHorariaVendedor}`);
-    }
-}
+    } */
+window.onload = consultarVendedor;
 document.querySelector('.btn-voltar').addEventListener('click', voltarPaginaInicial);
