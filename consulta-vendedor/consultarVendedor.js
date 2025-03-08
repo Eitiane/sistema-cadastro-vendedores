ec04fd4eb9952d414172df25717bc3740800496e
Eitiane Borges <eitianeborges@gmail.com>
2025-03-07 23:11:50 -0300

    implementação css na pág de consulta de vendedores

--- consulta-vendedor/consultarVendedor.js
+++ consulta-vendedor/consultarVendedor.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-let mensagemErroConsulta = 'Erro a consultar dados';
-LOCAL_STORAGE_KEY = 'vendedores';
+    let mensagemErroConsulta = 'Erro a consultar dados';
+    LOCAL_STORAGE_KEY = 'vendedores';
 
-function consultarVendedor() {
-    let vendedores = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
-    let tabela = document.getElementById('tabela-vendedores');
-    console.log("Tabela encontrada?", tabela); // Depuração
-
-vendedores.forEach(vendedores => {
-    let linha = document.createElement('tr');
-    linha.innerHTML = `
+    function consultarVendedor() {
+        let vendedores = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+        let tabela = document.getElementById('tabela-vendedores');
+        if (vendedores.length === 0) {
+            alert(mensagemErroConsulta);
+            return;
+        }
+        vendedores.forEach(vendedores => {
+            let linha = document.createElement('tr');
+            linha.innerHTML = `
         <td>${vendedores.nome}</td>
         <td>${vendedores.idadeVendedor}</td>
         <td>${vendedores.vendasVendedor}</td>
         <td>${vendedores.cargaHorariaVendedor}</td>
     `;
-    tabela.appendChild(linha);
-    
-});}
-consultarVendedor();
-document.querySelector('.btn-voltar').addEventListener('click', voltarPaginaInicial);
+            tabela.appendChild(linha);
+
+        });
+    }
+    consultarVendedor();
+    document.querySelector('.btn-voltar').addEventListener('click', voltarPaginaInicial);
 }); 
